e618f197f298f5e8e605dcc880bf7c9c8ad257ea
CSettle1 <court.settle@gmail.com>
2021-01-07 19:00:17 -0500

    updated scripts.js
    if applied this commit will have added a for loop for the pokemonslist

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
 let pokemonsList= [{name: 'charmander', height: '0.6', type: 'fire'},
 {name: 'caterpie', height: '0.3', type: 'bug'},
 {name: 'ekans', height: '2', type: 'poison'}]
+
+for (let i = 0; i < pokemonsList.length; i++) {
+    console.log(pokemonsList[i]); }
