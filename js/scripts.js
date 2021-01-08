fcb0753a77922862ad4b0917e6330bdbfb1ff855
CSettle1 <court.settle@gmail.com>
2021-01-08 11:55:56 -0500

    upadted scripts.js
    if applied this commit will have updated scripts.js using work done in repl to have pokemons print each on a  new line in the dom with "wow that's big" next to largest pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,20 @@ let pokemonsList= [{name: 'charmander', height: '0.6', type: 'fire'},
 {name: 'caterpie', height: '0.3', type: 'bug'},
 {name: 'ekans', height: '2', type: 'poison'}]
 
+
 for (let i = 0; i < pokemonsList.length; i++) {
-    console.log(pokemonsList[i]); }
+  if (pokemonsList[i].height > 1) {
+    document.write( '</br>' +
+      pokemonsList[i].name + "" + " (height: " + pokemonsList[i].height + ") " +
+        " -Wow, that's big! "
+    );
+  } else {
+   document.write( '</br>' +
+      pokemonsList[i].name +
+        "" +
+        " (height: " +
+        pokemonsList[i].height +
+        ")"
+    );
+  }
+}
