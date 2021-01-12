8bc2c7d95ee36205d3e8a4f151002d5bbf2408b0
CSettle1 <court.settle@gmail.com>
2021-01-11 20:44:59 -0500

    updated scripts.js
    if applied this commit will have added a foreach function to loop over the pokemons list array and print the details to the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,10 @@
-let pokemonsList= [{name: 'charmander', height: '0.6', type: 'fire'},
+let pokemonsList=
+[{name: 'charmander', height: '0.6', type: 'fire'},
 {name: 'caterpie', height: '0.3', type: 'bug'},
+
 {name: 'ekans', height: '2', type: 'poison'}]
 
-
-for (let i = 0; i < pokemonsList.length; i++) {
-  if (pokemonsList[i].height > 1) {
-    document.write( '<p>' +
-      pokemonsList[i].name + "" + " (height: " + pokemonsList[i].height + ") " +
-        " -Wow, that's big! "
-    );
-  } else {
-   document.write( '</p>' +
-      pokemonsList[i].name +
-        "" +
-        " (height: " +
-        pokemonsList[i].height +
-        ")"
-    );
-  }
+function myLoopFunction(pokemon) {
+  console.log(pokemon.name + " " + pokemon.height + " " + pokemon.type);
 }
+pokemonsList.forEach(myLoopFunction);
