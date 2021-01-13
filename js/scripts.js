6f0570af7d9ed88013a485b584333d9a583295ae
CSettle1 <court.settle@gmail.com>
2021-01-13 08:46:13 -0500

    updated index.html
    if applied this commit will have updated index.html to fix the indentation

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
 var pokemonRepository = (function () {
-var pokemonList = [
-  {
-    name: 'charmander',
-    height: '0.6',
-    type: 'fire'
+  var pokemonList = [
+    {
+      name: 'charmander',
+      height: '0.6',
+      type: 'fire'
     },
     {
-  name: 'caterpie',
-  height: '0.3',
-  type: 'bug'
-  },
-  {
-    name: 'ekans',
-    height: '2',
-    type: 'poison'
+      name: 'caterpie',
+      height: '0.3',
+      type: 'bug'
+    },
+    {
+      name: 'ekans',
+      height: '2',
+      type: 'poison'
     }
-    ];
-function getAll() {
-  return pokemonList;
-}
-function add(pokemon) {
-  pokemonList.push(pokemon);
-}
-return {
-  getAll: getAll,
-  add: add,
-};
+  ];
+  function getAll() {
+    return pokemonList;
+  }
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  return {
+    getAll: getAll,
+    add: add,
+  };
 })();
 
 console.log(pokemonRepository.getAll() );
