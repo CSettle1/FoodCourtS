2d13dabaf5969ce5462f5e07e1e1388fa75922b3
CSettle1 <court.settle@gmail.com>
2021-01-15 17:19:02 -0500

    updated script.js
    if applied this commit will have changed the name of the repository, added a function of add pokemon, added the add list item with event listeners and buttons, and removed document.write

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var pokemonRepository = (function () {
-  var pokemonList = [
+  let repository = [
     {
       name: 'charmander',
       height: '0.6',
@@ -16,15 +16,41 @@ var pokemonRepository = (function () {
       type: 'poison'
     }
   ];
-  function getAll() {
-    return pokemonList;
-  }
+
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "type" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
+    }
+  }
+  function getAll() {
+    return repository;
+  }
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class")
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    button.addEventListener("click", function (event) {
+      showDetails(pokemon);
+    });
+  }
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
   }
   return {
     getAll: getAll,
     add: add,
+    addListItem: addListItem,
   };
 })();
 
@@ -33,5 +59,5 @@ console.log(pokemonRepository.getAll() );
 //console.log(pokemonRepository.getAll());
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-  document.write(pokemon.name + " type: " + pokemon.type + " height " + pokemon.height + "<br>");
+  pokemonRepository.addListItem(pokemon);
 });
